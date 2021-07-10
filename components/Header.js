b73cb83f60a082d1f097483f513509b5742df9a4
Obed Mulumba <ommartiste007@gmail.com>
2021-07-10 18:28:25 +0200

    added segond section with  documents list

--- components/Header.js
+++ components/Header.js
@@ -39,7 +39,7 @@ function Header() {
 
             <img 
                 loading="lazy"
-                className="cursor-pointer h-12 w-12 rounded-full ml-2"
+                className="hidden cursor-pointer h-12 w-12 rounded-full ml-2"
                 src="https://africanexperts.neocities.org/assets/img/dummies/team1.jpg"
                 alt=""
             />
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Button from '@material-tailwind/react/Button';
 import Icon from '@material-tailwind/react/Icon';
 import Head from 'next/head';
 import Header from '../components/Header'
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -28,6 +29,28 @@ export default function Home() {
             </Button>
           </div>
 
+          <div>
+            <div className="relative h-52 w-40 border-2 cursor-2 cursor-pointer hover:border-blue-700">
+              <Image src="https://links.papareact.com/pju" layout="fill" />
+            </div>
+
+            
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">
+              Vide
+            </p>
+          </div>
+
+        </div>
+      </section>
+
+
+      <section className="bg-white px-10 md:px-0">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+          <div className="flex items-center justify-between pb-5">
+            <h2 className="font-medium flex-grow">Mes documents</h2>
+            <p className="mr-12">Date de creation</p>
+            <Icon name="folder" size="3xl" color="gray" />
+          </div>
         </div>
       </section>
 
